bfc92410a795ede1ae531a8b24ec0785731f6184
isaev <isaev.denis.igorevih@gmail.com>
2025-09-08 08:59:54 +0300

    modified:   mongo_connect_check.php

--- mongo_connect_check.php
+++ mongo_connect_check.php
@@ -5,12 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 try {
     // Подключение к MongoDB с указанием базы аутентификации
-    $client = new MongoDB\Client(
-        'mongodb://aiwpre:ewfwef90ewfvf@20.56.52.242:27017/aiwpre?authSource=aiwpre'
-    );
-
-    // Выбор базы
-    $db = $client->aiwpre;
+    $connection = new \MongoDB\Client($_SERVER['CONNECTION_STRING']);
+	$db = $connection->selectDatabase($_SERVER['DATABASE']);
 
     // Получаем список коллекций
     $collections = $db->listCollections();
